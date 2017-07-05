219f7d1a3f97f40b8a2af18fb69c8e8b102025aa
algosig <sigdelsanjog@gmail.com>
2017-07-05 17:12:49 +0545

    Title Bar Implemented

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import 'jquery';
 import { Car, Drone } from './classes/index.js'
-import {fleet} from './services/fleet-data.js'
+import { fleet } from './services/fleet-data.js'
 import { FleetDataService } from './services/fleet-data-service.js'
 import { Button } from './ui/button.js'
+import { Image } from './ui/image.js'
+import { TitleBar } from './ui/title-bar.js'
+
+let tb = new TitleBar('Application')
+tb.appendToElement($('body'))
+
+let i = new Image('src/image/drone.jpg')
+i.appendToElement($('body'))
 
 let b = new Button('click')
 b.appendToElement($('body'))
+
