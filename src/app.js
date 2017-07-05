4e2f5f121a545d94f29fe357946800c5604eef71
algosig <sigdelsanjog@gmail.com>
2017-07-05 19:08:41 +0545

    Data Table added

--- src/app.js
+++ src/app.js
@@ -5,8 +5,13 @@ import { FleetDataService } from './services/fleet-data-service.js'
 import { Button } from './ui/button.js'
 import { Image } from './ui/image.js'
 import { TitleBar } from './ui/title-bar.js'
+import { DataTable } from './ui/data-table.js'
 
 let tb = new TitleBar('Application')
+tb.addLink('Home','')
+tb.addLink('Drones','')
+tb.addLink('Cars','')
+tb.addLink('About','')
 tb.appendToElement($('body'))
 
 let i = new Image('src/image/drone.jpg')
@@ -14,4 +19,11 @@ i.appendToElement($('body'))
 
 let b = new Button('click')
 b.appendToElement($('body'))
+
+let headers = "License Make Model Miles".split(' ')
+let dataService = new FleetDataService()
+dataService.loadData(fleet)
+
+let dt = new DataTable(headers, dataService.cars)
+dt.appendToElement($('body'))
 
