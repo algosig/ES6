4e16f109072ff992d20cedfb13730d5ab20ec077
algosig <sigdelsanjog@gmail.com>
2017-07-05 15:20:11 +0545

    1) User Interface Class added 2) jquery installed via npm 3) material-design-lite installed via npm' '

--- src/app.js
+++ src/app.js
@@ -1,27 +1,8 @@
+import 'jquery';
 import { Car, Drone } from './classes/index.js'
 import {fleet} from './services/fleet-data.js'
 import { FleetDataService } from './services/fleet-data-service.js'
+import { Button } from './ui/button.js'
 
-let dataService = new FleetDataService()
-dataService.loadData(fleet)
-
-
-let cars = dataService.filterCarsByMake('U')
-
-for (let car of cars) 
-    console.log(car.make)
-
-// let car = dataService.getCarByLicense('A5')
-// console.log(car)
-
-// let cars = dataService.getCarsSortedByLicense();
-// for (let car of cars)
-//     console.log(car.license)
-
-
-// for (let car of dataService.cars) {
-//     console.log(car.license)
-// }
-// for (let e of dataService.errors) {
-//     console.log(e.message)
-// }
+let b = new Button('click')
+b.appendToElement($('body'))
