544b032329c6c74daf165c121544a4044b80ce6d
algosig <sigdelsanjog@gmail.com>
2017-07-06 12:31:23 +0545

    Google Maps API added Successfully

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import { Button } from './ui/button.js'
 import { Image } from './ui/image.js'
 import { TitleBar } from './ui/title-bar.js'
 import { DataTable } from './ui/data-table.js'
+import {GoogleMap} from './ui/google-map.js'
 
 let tb = new TitleBar('Application')
 tb.addLink('Home','')
@@ -26,4 +27,8 @@ dataService.loadData(fleet)
 
 let dt = new DataTable(headers, dataService.cars)
 dt.appendToElement($('body'))
+
+let centreOfMap={lat: 40.777777, lng:-73.989898}
+let map = new GoogleMap(centreOfMap, dataService.cars )
+map.appendToElement($('body'))
 
