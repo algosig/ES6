e15ac5a892200655f46f1d597104111546e532cc
algosig <sigdelsanjog@gmail.com>
2017-07-06 11:32:02 +0545

    Data Table Implemented. Time for imeplementing GOOGLE MAP API

---
+++ src/ui/base-element.js
@@ -0,0 +1,25 @@
+import 'jquery';
+export class BaseElement {
+    constructor() {
+        this.element = null //jQuery Object
+    }
+
+    appendToElement(el) {
+        this.createElement()
+        el.append(this.element)
+        this.enableJS()
+    }
+
+
+    createElement() {
+        let s = this.getElementString()
+        this.element = $(s)
+    }
+    getElementString() {
+        throw 'Please override getElementString() in BaseElement'
+    }
+
+    enableJS() {
+        componentHandler.upgradeElement(this.element[0]);
+    }
+}
