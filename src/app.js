89d52db6ae3d39c500bbc6a6f0e36c93213988ff
algosig <sigdelsanjog@gmail.com>
2017-07-06 16:05:31 +0545

    Map Page added and Google map Displayed on that Page

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import { DataTable } from './ui/data-table.js'
 import {GoogleMap} from './ui/google-map.js'
 import { HomePage } from './home-page.js'
 import { CarsPage } from './cars-page.js'
+import { MapPage } from './map-page.js'
 import { ApplicationBase } from './framework/application-base.js'
 
 export class App extends ApplicationBase {
@@ -18,33 +19,11 @@ export class App extends ApplicationBase {
         this.addRoute('Home', new HomePage(), true)
         this.addRoute('Cars', new CarsPage())
         this.addRoute('Drones', null)
-        this.addRoute('Map', null)
+        this.addRoute('Map', new MapPage())
 
     }
 }
 export let application = new App()
 application.show($('body'))
-// application.shwo($('body'))
-// let tb = new TitleBar('Application')
-// tb.addLink('Home','')
-// tb.addLink('Drones','')
-// tb.addLink('Cars','')
-// tb.addLink('About','')
-// tb.appendToElement($('body'))
 
-// let i = new Image('src/image/drone.jpg')
-// i.appendToElement($('body'))
-
-// let b = new Button('click')
-// b.appendToElement($('body'))
-
-// let headers = "License Make Model Miles".split(' ')
-
-
-// let dt = new DataTable(headers, dataService.cars)
-// dt.appendToElement($('body'))
-
-// let centreOfMap={lat: 40.777777, lng:-73.989898}
-// let map = new GoogleMap(centreOfMap, dataService.cars )
-// map.appendToElement($('body'))
 
